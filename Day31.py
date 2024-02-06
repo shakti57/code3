8ccfe592fae9300465a69f37f04a9482e2229a9d
Shakti Ranjan Mohanta <153294330+shakti57@users.noreply.github.com>
2024-02-06 16:09:32 +0530

    Add files via upload

---
+++ Day31.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def reverse_string(input_string):
+    return input_string[::-1]
+
+# Example usage:
+original_string = "Hello, world!"
+reversed_string = reverse_string(original_string)
+print("Original:", original_string)
+print("Reversed:", reversed_string)
+
+
+# In[2]:
+
+
+def reverse_number(number):
+    # Convert number to a string, reverse it, and convert it back to an integer
+    reversed_number = int(str(number)[::-1])
+    return reversed_number
+
+# Example usage:
+original_number = 12345
+reversed_number = reverse_number(original_number)
+print("Original number:", original_number)
+print("Reversed number:", reversed_number)
+
+
+
+# In[ ]:
+
+
+
+
