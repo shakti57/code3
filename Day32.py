b9b61749fe1d973f285c742255fa3509e7ac1b52
Shakti Ranjan Mohanta <153294330+shakti57@users.noreply.github.com>
2024-02-08 12:45:23 +0530

    Add files via upload

---
+++ Day32.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+# Create a sample DataFrame
+data = {'A': [1, 2, 3, 4],
+        'B': [5, 6, 7, 8],
+        'C': [9, 10, 11, 12]}
+df = pd.DataFrame(data)
+
+print("Original DataFrame:")
+print(df)
+
+# Drop a column
+column_to_drop = 'B'
+df = df.drop(column_to_drop, axis=1)
+
+print("\nDataFrame after dropping column '{}'".format(column_to_drop))
+print(df)
+
+# Drop a row
+row_to_drop = 2
+df = df.drop(row_to_drop)
+
+print("\nDataFrame after dropping row '{}'".format(row_to_drop))
+print(df)
+
+
+# In[ ]:
+
+
+
+
